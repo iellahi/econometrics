89d6099c142eec1ff2eccb0b2476e999c5e9d3dc
Ibraheem Saqib Ellahi <67660287+iellahi@users.noreply.github.com>
2025-07-15 02:36:34 +0500

    Update project.R

--- project.R
+++ project.R
@@ -2,7 +2,7 @@
 # ECO4421 Econometrics 
 
 
-# 1 midterms and attendance ***
+# 1 - midterms and attendance ***
 # Observation: +ve correlation but not enough variation in attendance to produce stron results
 
 mid_att <- lm(avg_midterm ~ excercises, data=microdata)
@@ -10,7 +10,7 @@ summary(mid_att)
 plot(microdata$excercises, microdata$avg_midterm)
 abline(lm(microdata$avg_midterm ~ microdata$excercises))
 
-# 2 psets and attendance
+# 2 - psets and attendance
 # Observation: +ve correlation but not enough variation in attendance to produce strong results
 
 pset_att <- lm(avg_pset ~ attendance, data=microdata)
@@ -19,7 +19,7 @@ summary(att_pset)
 plot(microdata$avg_pset, microdata$attendance)
 abline(lm(microdata$avg_pset ~ microdata$attendance))
 
-# 3 midterms and psets ***
+# 3 - midterms and psets ***
 # Observation: uncertain?? doesnt seem to be a very strong trend?
 
 mid_pset <- lm(avg_midterm ~ avg_pset, data=microdata)
@@ -31,7 +31,7 @@ abline(lm(microdata$avg_midterm ~ microdata$avg_pset))
 multreg <- lm(avg_midterm ~ excercises+avg_pset+year, data=microdata)
 summary(multreg)
 
-# 4 midterms and year ***
+# 4 - midterms and year ***
 # Observation: Looking at the graph, Juniors generally score higher than Seniors; not enough data on Sophomores
 
 mid_year <- lm(avg_midterm ~ year, data=microdata)
@@ -39,14 +39,14 @@ summary(mid_year)
 plot(microdata$year, microdata$avg_midterm)
 abline(lm(microdata$avg_midterm ~ microdata$year))
 
-# 5 midterms and major
+# 5 - midterms and major
 # Observation: no clear difference, except that top scorers tend to be econ majors; no clear result on STEM majors scoring higher (act sci scored 18 but IT scored 14)
 
 mid_maj <- lm(avg_midterm ~ economics, data=microdata)
 summary(mid_maj)
 plot(microdata$economics, microdata$avg_midterm)
 
-# midterm1 and midterm2
+# 6 - midterm1 and midterm2
 # Observation: Clear +ve correlation between performance in both midterms
 
 mid1_mid2 <- lm(midterm1 ~ midterm2, data=microdata)
